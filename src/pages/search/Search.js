ea292df5d9f8f027b6683568bd2043b189940ec1
Vatsal <vatsalpandey2110@gmail.com>
2022-09-28 21:09:32 +0530

    final commit

---
+++ src/pages/search/Search.js
@@ -0,0 +1,22 @@
+// styles
+import "./Search.css";
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import RecipeList from "../../components/RecipeList";
+export default function Search() {
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+
+  const url = "http://localhost:3000/recipes?q=" + query;
+  const { data, isPending, error } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
+}
